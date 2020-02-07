47f4e5165b0c59a2bd94829dc25c218c7fefc0f1
rfacey <roberto.facey@gmail.com>
2020-02-07 12:58:29 -0600

    Update 20200207

--- Learn Python 3 The Hard Way/Examples/ex26.py
+++ Learn Python 3 The Hard Way/Examples/ex26.py
@@ -11,16 +11,18 @@ weight = input()
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 txt = open(filename)
+fileread = txt.read()
 
 print(f"Here's your file {filename}:")
-print(txt)
+print(fileread)
 
 print("Type the filename again:")
-file_again = input("> ")
+txt_input = input("> ")
 
-txt_again = open(file_again)
+txt_again = open(txt_input)
+fileread_again = txt_again.read()
 
-print(txt_again)
+print(fileread_again)
 
 
 print("Let\'s practice everything.")
@@ -54,14 +56,14 @@ start_point = 10000
 jelly_beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
-print("With a starting point of: {}".format(*start_point))
+print("With a starting point of: {}".format(start_point))
 # it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+print(f"We'd have {jelly_beans} jellybeans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
